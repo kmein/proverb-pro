63ec3aa3bd0161b565134afdefedf36801728d63
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2019-04-14 21:50:56 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(
     name="telegram-proverb",
     version="0.1.0",
     scripts=["./proverb_bot.py"],
+    py_modules=["proverb_pro"],
     install_requires=["pillow", "beautifulsoup4", "telepot"],
 )
